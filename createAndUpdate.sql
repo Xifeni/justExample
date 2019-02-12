7700d250f13173764cf99fdc94f9338ca81bb47d
2b <1111>
2019-02-12 19:14:08 +0300

    temp commit

--- createAndUpdate.sql
+++ createAndUpdate.sql
@@ -1,38 +1,20 @@
 create table users
 (
-  username  varchar(32)  not null
-    primary key,
-  firstname varchar(128) null,
-  lastname  varchar(128) null
+  username    varchar(32)        null,
+  firstname   varchar(32)        null,
+  lastname    varchar(32)        null,
+  id          int                not null,
+  password    varchar(64)        null,
+  permissions int(4) default 100 null,
+  constraint users_id_uindex
+    unique (id)
 );
 
-create table vault
+alter table users
+  add primary key (id);
+
+create table sessions
 (
-  username        varchar(32) not null
-    primary key,
-  password        varchar(64) not null,
-  current_session varchar(32) null,
-  constraint vault_users_username_fk
-    foreign key (username) references users (username)
-      on update cascade on delete cascade
+  id              int         not null,
+  current_session varchar(64) null
 );
-
-create table permission
-(
-  username   varchar(32)  not null
-    primary key,
-  permission varchar(4)   null,
-  rolename   varchar(128) null,
-  constraint permission_users_username_fk
-    foreign key (username) references users (username)
-      on update cascade on delete cascade
-);
-
-INSERT INTO users (username, firstname, lastname) VALUES ('test', 'test', 'test');
-INSERT INTO users (username, firstname, lastname) VALUES ('user', 'user', 'user');
-
-INSERT INTO permission (username, permission, rolename) VALUES ('test', '111', 'test');
-INSERT INTO permission (username, permission, rolename) VALUES ('user', '100', 'user');
-
-INSERT INTO vault (username, password, current_session) VALUES ('test', 'edd5bb5389dfb55beb4d3ac56b7546c17cd464bc4da27aaf95b7dc9cbae8cf1f', null);
-INSERT INTO vault (username, password, current_session) VALUES ('user', '48cee8d8b352a2221a8993ac5fafd739ef877013a7576e582ac1363b92995782', null);
